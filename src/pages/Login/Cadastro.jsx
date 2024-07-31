abb48f6a3f228b54261456e6ee00243721bd48f9
DevGSBraga <gabrielbraga0710@gmail.com>
2024-07-31 20:29:32 -0300

    Update Cadastro.jsx
    url update

--- src/pages/Login/Cadastro.jsx
+++ src/pages/Login/Cadastro.jsx
@@ -40,7 +40,7 @@ const Cadastro = () => {
 
         if(validaCadastro()) {
             try {
-                const resposta = await axios.post('http://localhost:3000/auth/user', {
+                const resposta = await axios.post('https://jspresso-coffe-house.vercel.app/cadastro', {
                     email,
                     password: senha,
                     confirmPassword: confirmaSenha
